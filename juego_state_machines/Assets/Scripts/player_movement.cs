810eb28bee422b1c3d50fdc33c29851f0cae3d14
Sarduc <franimpe@outlook.es>
2022-11-06 14:42:19 -0300

    setup

---
+++ juego_state_machines/Assets/Scripts/player_movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    int speed = 5;
+    bool sex = 2 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
